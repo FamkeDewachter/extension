28ba42fd251ed4f5f338859056ccc8b3838b9605
FamkeDewachter <famke.dewachter@hotmail.be>
2025-03-06 22:37:46 +0100

    add commetns expaining more

--- background.js
+++ background.js
@@ -1,10 +1,14 @@
+// This event listener is triggered when the extension is installed.
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Google Drive Extension Installed");
 });
 
+// This event listener is triggered when the extension's action button is clicked.
 chrome.action.onClicked.addListener(async (tab) => {
   try {
+    // Get the authentication token.
     const token = await getAuthToken();
+    // List the files in Google Drive using the obtained token.
     const files = await listDriveFiles(token);
     console.log(files);
   } catch (error) {
@@ -12,25 +16,33 @@ chrome.action.onClicked.addListener(async (tab) => {
   }
 });
 
+// Function to get the authentication token.
 async function getAuthToken() {
   return new Promise((resolve, reject) => {
+    // Request an OAuth token from the Chrome Identity API.
     chrome.identity.getAuthToken({ interactive: true }, (token) => {
       if (chrome.runtime.lastError) {
+        // Reject the promise if there is an error.
         reject(chrome.runtime.lastError);
       } else {
+        // Resolve the promise with the obtained token.
         resolve(token);
       }
     });
   });
 }
 
+// Function to list the files in Google Drive.
 async function listDriveFiles(token) {
+  // Make a GET request to the Google Drive API to list files.
   const response = await fetch("https://www.googleapis.com/drive/v3/files", {
     method: "GET",
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
+  // Parse the response as JSON.
   const data = await response.json();
+  // Return the list of files.
   return data.files;
 }
--- popup.js
+++ popup.js
@@ -1,25 +1,35 @@
+// Add an event listener to the authorize button to handle click events
 document
   .getElementById("authorizeButton")
   .addEventListener("click", async () => {
     try {
+      // Get the authentication token
       const token = await getAuthToken();
+      // List the files from Google Drive using the token
       const files = await listDriveFiles(token);
+      // Get the file list element from the DOM
       const fileListElement = document.getElementById("fileList");
+      // Populate the file list element with the names of the files
       fileListElement.innerHTML = files
         .map((file) => `<p>${file.name}</p>`)
         .join("");
     } catch (error) {
+      // Log any errors to the console and show an alert to the user
       console.error("Error during authorization or fetching files:", error);
       alert("An error occurred while interacting with Google Drive.");
     }
   });
 
+// Function to get the authentication token
 async function getAuthToken() {
   return new Promise((resolve, reject) => {
+    // Use the Chrome Identity API to get the auth token
     chrome.identity.getAuthToken({ interactive: true }, (token) => {
       if (chrome.runtime.lastError) {
+        // Reject the promise if there is an error
         reject(new Error(chrome.runtime.lastError.message));
       } else {
+        // Resolve the promise with the token
         resolve(token);
       }
     });
